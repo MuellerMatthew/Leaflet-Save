b42dfd4fd4405d6bd824af4f5999ae42bdb855a1
Matthew Mueller <MuellerMatthew@users.noreply.github.com>
2015-09-09 00:12:27 -0400

    Update leaflet-save.js

--- leaflet-save.js
+++ leaflet-save.js
@@ -1,13 +1,21 @@
 (function () {
   L.DomUtil.Save = L.DomUtil.Extend({
     options: {
-    
+      center          : false,
+      zoom            : false,
+      bounds          : false,
+      lastEvent       : false,
+      eventTypes      : "",
+      layerVisibility : false,
     },
     
     initialize: function(options) {
       L.Util.setOptions(this, options || {});
       
       
-    }
+    },
+    
+    
+    
   })
 })
